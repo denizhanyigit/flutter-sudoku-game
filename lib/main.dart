ff4842d21d0ba97e6b24a244635db833c4755364
Denizhan YIGIT <denizhan.yigit@hotmail.com>
2021-01-01 20:43:07 +0300

    4.ders

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
+import 'package:sudoku_game/giris_sayfasi.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter('sudoku');
+  // Box => sql veritabanları tablolara denk gelir.
+  await Hive.openBox('ayarlar');
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+    return ValueListenableBuilder<Box>(
+        valueListenable:
+            Hive.box('ayarlar').listenable(keys: ['karanlik_tema', 'dil']),
+        builder: (context, kutu, _) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: kutu.get('karanlik_tema', defaultValue: false)
+                ? ThemeData.dark()
+                : ThemeData.light(),
+            /* theme: ThemeData(
+                textTheme: GoogleFonts.lobsterTextTheme(),
+                appBarTheme:
+                    AppBarTheme(textTheme: GoogleFonts.lobsterTextTheme())), */
+            home: GirisSayfasi(),
+          );
+        });
   }
 }
